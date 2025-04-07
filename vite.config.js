737776075aeb623ce2306c1978fe20e192f8291b
Pipidastr05 <uap5977@gmail.com>
2025-04-08 03:20:17 +0700

    Исправила ошибку с виселицей и стилями?

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@
 
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base:'/College-Open-Day/'
+  base:'/College-Open-Day/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        hangman: path.resolve(__dirname, 'hangman-game.html'),
+      }
+    }
+  }
 })
