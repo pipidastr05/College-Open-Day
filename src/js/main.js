712d1eb862104209394a04cda47339bdabadc609
Pipidastr05 <uap5977@gmail.com>
2025-04-11 00:37:13 +0700

    Подключила index к supabase

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@ import '../css/styles-main.css';
 
 import { darkModeHandle } from './utils';
 import { startGame } from './game';
+import { getUserId } from './statistic';
 
 
 darkModeHandle();
+getUserId();
 
 const startGameButton = document.getElementById('startGame');
 startGameButton.addEventListener('click', startGame)
